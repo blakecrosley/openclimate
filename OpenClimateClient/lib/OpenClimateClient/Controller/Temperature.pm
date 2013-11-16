7a03bac696564edb1e9df46542595cdb39d2fc74
Grigor Karavardanyan <k.grigor@yahoo.com>
2013-11-16 14:02:50 -0800

    Temperature REST controller that will receive requests and store in db.

---
+++ OpenClimateClient/lib/OpenClimateClient/Controller/Temperature.pm
@@ -0,0 +1,47 @@
+package OpenClimateClient::Controller::Temperature;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+OpenClimateClient::Controller::Temperature - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+
+    $c->response->body('Matched OpenClimateClient::Controller::Temperature in Temperature.');
+}
+
+
+
+=encoding utf8
+
+=head1 AUTHOR
+
+Grigor Karavardanyan
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
